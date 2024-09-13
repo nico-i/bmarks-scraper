3afcbd7d4546a0d621ab4722da434d71d251262e
Nico Ismaili <nico.ismaili@aoe.com>
2024-09-13 13:58:22 +0200

    feat: added unit tests
    /spend 1h

--- src/domain/value_objects/whitelist/test_Whitelist.py
+++ src/domain/value_objects/whitelist/test_Whitelist.py
@@ -1,6 +1,26 @@
 import os
 import pytest
 
+from domain.repositories import whitelist
+from domain.value_objects.whitelist.Whitelist import Whitelist
+from infrastructure.persistance.adapters.whitelist.fs.FSWhitelistRepo import FSWhitelistRepo
+
+mock_whitelist_path = f"{os.path.dirname(__file__)}/__mocks__/.bkmks"
+
 class TestWhitelist:
     def test_is_whitelisted(self):
-        raise NotImplementedError("Test not implemented")
+        with open(mock_whitelist_path, "r", encoding="utf-8") as f:
+            whitelist_file_content = f.read()
+
+        whitelist = Whitelist(whitelist_file_content=whitelist_file_content)
+        assert whitelist.is_whitelisted("Test Bookmark") == True
+        assert whitelist.is_whitelisted("Test Bookmark 2") == False
+        assert whitelist.is_whitelisted("root folder/") == False
+        assert whitelist.is_whitelisted("root folder/Test Bookmark") == True
+        assert whitelist.is_whitelisted("test/blacklisted") == False
+        assert whitelist.is_whitelisted("test/deep/") == False
+        assert whitelist.is_whitelisted("test/deep/folder/") == True
+        assert whitelist.is_whitelisted("test/deep/folder/Bookmark") == True
+        assert whitelist.is_whitelisted("test/deep/folder/blacklisted bookmark") == False
+        assert whitelist.is_whitelisted("test/deep/folder/another bookmark") == True
+        assert whitelist.is_whitelisted("test/deep/folder/blacklisted folder/") == False
--- src/infrastructure/persistance/adapters/bookmark/brave/test_BraveBookmarkRepo.py
+++ src/infrastructure/persistance/adapters/bookmark/brave/test_BraveBookmarkRepo.py
@@ -8,25 +8,39 @@ from infrastructure.persistance.adapters.whitelist.fs.FSWhitelistRepo import FSW
 
 mock_brave_export_path = f"{os.path.dirname(__file__)}/__mocks__/mock_brave_export.json"
 
-mock_whitelist_path = f"{os.path.dirname(__file__)}/__mocks__/.bkmks_wl"
+mock_whitelist_path = f"{os.path.dirname(__file__)}/__mocks__/.bkmks"
 
 class TestBraveBookmarkRepo:
 	def test_get_full_toolbar(self):
 		brave_bookmark_repo = BraveWhitelistedBookmarkRepo(brave_bookmarks_path=mock_brave_export_path)
 		full_toolbar = brave_bookmark_repo.get_root_folder()
-  
+
 		expected_bookmarks_folder = Folder(name="root", children=[
-			Folder(name="Dev", children=[
-				Folder(name="Inspiration", children=[Bookmark(name="Yasoob Khalid", url="https://yasoob.me/")]),
-				Bookmark(name="Backdrop Build", url="https://backdropbuild.com/v3/cv-ops"),
-			]),
+			Folder(name="whitelisted root level Folder", children=[ 
+				Folder(name="1. level whitelisted Folder", children=[
+					Bookmark(name="2. Level whitelisted Bookmark", url= "https://whitelist.com"),
+					Bookmark(name="2. Level blacklisted Bookmark", url= "https://blacklist.com"),
+				]),	
+				Bookmark(name="1. Level whitelisted Bookmark", url= "https://whitelist.com"),
+				Bookmark(name="1. Level blacklisted Bookmark", url= "https://blacklist.com")
+				]),
+			Bookmark(name="root level whitelisted bookmark", url= "https://whitelist.com"),
+			Bookmark(name="root level blacklisted bookmark", url= "https://blacklist.com"),
 		])
 
 		assert full_toolbar == expected_bookmarks_folder
-  
+
 	def test_get_whitelisted_toolbar(self):
 		whitelisted_repo = FSWhitelistRepo(whitelist_path=mock_whitelist_path)
 		brave_whitelisted_bookmark_repo = BraveWhitelistedBookmarkRepo(brave_bookmarks_path=mock_brave_export_path, whitelist_repo=whitelisted_repo)
 		whitelisted_toolbar = brave_whitelisted_bookmark_repo.get_whitelisted_root_folder()
 
-		raise NotImplementedError("Test not implemented")
+		assert whitelisted_toolbar == Folder(name="root", children=[
+			Folder(name="whitelisted root level Folder", children=[ 
+				Folder(name="1. level whitelisted Folder", children=[
+					Bookmark(name="2. Level whitelisted Bookmark", url= "https://whitelist.com"),
+				]),	
+				Bookmark(name="1. Level whitelisted Bookmark", url= "https://whitelist.com"),
+				]),
+			Bookmark(name="root level whitelisted bookmark", url= "https://whitelist.com"),
+		])
